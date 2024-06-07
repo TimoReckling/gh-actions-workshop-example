41c15cbc3c441dd1bf92b90b8fbc6eac2fdb8e3a
Timo Reckling <timo.reckling@tngtech.com>
2024-06-07 16:14:56 +0200

    [GHWS-4] make test red to see how that plays out

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('/gh-actions-workshop-example/');
 
-  await expect(page).toHaveTitle(/GitHub Actions Workshop/);
+  await expect(page).toHaveTitle(/GitHub Actions orkshop/);
 });
